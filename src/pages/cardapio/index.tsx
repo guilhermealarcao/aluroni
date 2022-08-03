82b51f3f21d5684b5096d116174efae98dc120ad
Guilherme <guilhermealarcao@gmail.com>
2022-08-03 17:02:23 -0300

    aula 2.6

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -1,5 +1,5 @@
 import stlyes from "./Cardapio.module.scss";
-import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
 export default function Cardapio() {
     return (
