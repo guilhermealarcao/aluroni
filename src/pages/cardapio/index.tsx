a7e7542abc9c87b87fc6f15482f6137f024a57a5
Guilherme <guilhermealarcao@gmail.com>
2022-08-03 16:54:20 -0300

    aula 2.1

---
+++ src/pages/cardapio/index.tsx
@@ -0,0 +1,9 @@
+export default function Cardapio(){
+    return(
+        <main>
+            <nav className={styles.menu}>
+                <img src=/>
+            </nav>
+        </main>
+    )
+}
